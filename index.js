21e543bff64f313d0ac2778f4181f2f3f7168dc9
lixue <13439414566@163.com>
2019-07-16 10:35:25 +0800

    xx

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+function component(){
+    var element = document.createElement('div');
+   // element.innerHTML="aa";
+    return element;
+}
+
+document.body.appendChild(component());
---
+++ js/common.js
@@ -0,0 +1,13 @@
+$({ property: 0 }).animate({ property: 100 }, {
+    duration: 3000,
+    step: function () {
+        var percentage = Math.round(this.property);
+
+        $('#progress').css('width', percentage + "%");
+
+        //  if(percentage == 100) {
+        // 	 $("#progress").addClass("done");//完成，隐藏进度条
+        //  }
+    }
+});
+
